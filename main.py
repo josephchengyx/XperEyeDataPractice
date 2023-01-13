a225641b7e3ad8e6159475f280959531e0117f81
Julie <hwangjy90@gmail.com>
2023-01-13 17:10:14 -0500

    using arb values for mm2pixel & filtering out certain timeframe

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 from DBUtil import create_server_connection, get_column_from_table
-from UnitConverter import deg2mm_coord_xy
+from UnitConverter import deg2mm_coord_xy, mm2pixel
 
 
 def get_rows_from_table(cnx, colnames, table):
@@ -56,25 +56,31 @@ if __name__ == "__main__":
     # TODO: get the corresponding taskid
     # TODO: get the corresponding image
 
+    #print(device_msg_list)
     left_eye_deg = []
     right_eye_deg = []
     for l in device_msg_list:
-        if l['id'] == 'leftIscan':
-            left_eye_deg.append(l['degree'])
-        elif l['id'] == 'rightIscan':
-            right_eye_deg.append(l['degree'])
+        if int(l['timestamp']) > 1673646612905728 and int(l['timestamp']) < 1673646617905947:
+            if l['id'] == 'leftIscan':
+                left_eye_deg.append(l['degree'])
+            elif l['id'] == 'rightIscan':
+                right_eye_deg.append(l['degree'])
 
 
     left_eye_mm = deg2mm_coord_xy(left_eye_deg, screen_distance_mm)
     right_eye_mm = deg2mm_coord_xy(right_eye_deg, screen_distance_mm)
 
+    # TODO: CHANGE MM TO PIXEL
     im = plt.imread("2.JPG")
     pixel_width = im.shape[0]
     pixel_height = im.shape[1]
 
+    left_eye_mm = mm2pixel(1016, 572, 3840, 2160, left_eye_mm)
+    #right_eye_mm = mm2pixel(76, 114, pixel_width, pixel_height, right_eye_mm)
+
     plt.imshow(im, extent=[-im.shape[1] / 2., im.shape[1] / 2., -im.shape[0] / 2., im.shape[0] / 2.])
     plt.scatter(left_eye_mm['x'],left_eye_mm['y'], linestyle='-', marker='.', c = 'red')
-    plt.scatter(right_eye_mm['x'],right_eye_mm['y'], linestyle='-', marker='.', c = 'blue')
+    #plt.scatter(right_eye_mm['x'],right_eye_mm['y'], linestyle='-', marker='.', c = 'blue')
     plt.show()
 
     connection.close()
